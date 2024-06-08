b69a5aaf0910103b9fb357b516c6ae06387fa8d0
Kevin Patel <patelkevin1210@gmail.com>
2024-06-08 20:29:16 +0530

    Initial commit with fix of AverageMoonGravity

---
+++ Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        public float AverageMoonGravity
+        {
+            get;
+            set;
+        }
+
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Moons = new Collection<Moon>();
+            if(planetDto.Moons != null)
+            {
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    //Addition of gravity value for each moon
+                    AverageMoonGravity += moonDto.Gravity;
+                    Moons.Add(new Moon(moonDto));
+                }
+                //Calculating average moon gravity value (upto 3 decimal)
+                AverageMoonGravity = (float)Math.Round(AverageMoonGravity / planetDto.Moons.Count, 3);
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
